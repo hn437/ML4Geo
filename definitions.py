f00592a13b9870e4f783c477aab43924ca1114a0
Danner, Jonas <Danner@stud.uni-heidelberg.de>
2021-08-18 17:03:00 +0200

    added sub_paths for test and train

--- definitions.py
+++ definitions.py
@@ -9,12 +9,19 @@ import yaml
 
 OHSOME_API = os.getenv("OHSOME_API", default="https://api.ohsome.org/v1/")
 DATA_PATH = "./data"
-Path(DATA_PATH).mkdir(parents=True, exist_ok=True)
+RASTER_PATH = os.path.join(DATA_PATH, "testRaster.tif")
 
 TRAINING_PATH = os.path.join(DATA_PATH, "training_data")
+TRAINING_PATH_IMG = os.path.join(TRAINING_PATH, "img")
+TRAINING_PATH_MASK = os.path.join(TRAINING_PATH, "mask")
 TEST_PATH = os.path.join(DATA_PATH, "test_data")
-RASTER_PATH = os.path.join(DATA_PATH, "testRaster.tif")
+TEST_PATH_IMG = os.path.join(TEST_PATH, "img")
+TEST_PATH_MASK = os.path.join(TEST_PATH, "mask")
 INTERMEDIATE_PATH = os.path.join(DATA_PATH, "intermediate_result")
+paths = [DATA_PATH, TRAINING_PATH, TRAINING_PATH_IMG, TRAINING_PATH_MASK, TEST_PATH, TEST_PATH_IMG, TEST_PATH_MASK, INTERMEDIATE_PATH]
+
+for path in paths:
+    Path(path).mkdir(parents=True, exist_ok=True)
 
 
 def get_logger():

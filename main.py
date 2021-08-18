4e4e37b0f4ce9d8134ef42336c3a37b54fe5ab9e
Danner, Jonas <Danner@stud.uni-heidelberg.de>
2021-08-18 11:56:04 +0200

    added raster reprojection, fixed bbox bug in buildings query, added mask generation

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
 import geopandas as gpd
+from shapely.geometry import box
+import shapely
 import rasterio
 from rasterio import mask
+from rasterio.warp import calculate_default_transform, reproject, Resampling
+import json
+import numpy as np
 
 
-from definitions import TRAINING_PATH, TEST_PATH, RASTER_PATH
+from definitions import TRAINING_PATH, TEST_PATH, RASTER_PATH, INTERMEDIATE_PATH
 from utils import query
 buildings_path = None
 
@@ -21,18 +26,62 @@ def setup_folders():
         os.mkdir(TRAINING_PATH)
     if not os.path.exists(TEST_PATH):
         os.mkdir(TEST_PATH)
+    if not os.path.exists(INTERMEDIATE_PATH):
+        os.mkdir(INTERMEDIATE_PATH)
+
+
+def reproject_raster():
+    dst_crs = 'EPSG:4326'
+    with rasterio.open(RASTER_PATH) as src:
+        transform, width, height = calculate_default_transform(
+            src.crs, dst_crs, src.width, src.height, *src.bounds)
+        kwargs = src.meta.copy()
+        kwargs.update({
+            'crs': dst_crs,
+            'transform': transform,
+            'width': width,
+            'height': height
+        })
+
+        with rasterio.open(os.path.join(INTERMEDIATE_PATH, "reprojected_raster.tif"), 'w', **kwargs) as dst:
+            for i in range(1, src.count + 1):
+                reproject(
+                    source=rasterio.band(src, i),
+                    destination=rasterio.band(dst, i),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=transform,
+                    dst_crs=dst_crs,
+                    resampling=Resampling.nearest)
+
+
+
+    return None
 
 
 def get_building_data(raster):
-    bbox = raster.bounds
+    bbox = box(*raster.bounds)
+    bbox = gpd.GeoSeries([bbox]).__geo_interface__
+    bbox = json.dumps(bbox)
     buildings = query(buil_def, bbox)
     return buildings
 
 
 def generate_mask(raster, vector):
-    r_mask = mask.mask(raster, vector, all_touched=False, invert=False, nodata=None, filled=True, crop=False,
+    out_image, out_transform = mask.mask(raster, vector, all_touched=False, invert=False, nodata=None, filled=True, crop=False,
                        pad=False, pad_width=0.5, indexes=None)
-    return r_mask
+    out_image = out_image[0] + out_image[1] + out_image[2]
+    out_image = np.where(out_image > 0, 1, out_image)
+    out_meta = raster.meta
+    out_meta.update({"driver": "GTiff",
+                     "count": int(1),
+                     "height": out_image.shape[0],
+                     "width": out_image.shape[1],
+                     "transform": out_transform})
+
+    with rasterio.open(os.path.join(INTERMEDIATE_PATH, "masked_raster.tif"), "w", **out_meta) as dest:
+        dest.write(out_image, 1)
+    return out_meta
 
 
 def create_ml_data(raster, r_mask, vector):
@@ -48,11 +97,17 @@ def create_ml_data(raster, r_mask, vector):
 
 def main():
     setup_folders()
-    raster = rasterio.open(RASTER_PATH)
+    reproject_raster()
+    raster = rasterio.open(os.path.join(INTERMEDIATE_PATH, "reprojected_raster.tif"))
     buildings = get_building_data(raster)
     buildings = gpd.GeoDataFrame.from_features(buildings["features"])
 
-    r_mask = generate_mask(raster, buildings)
+    r_mask = generate_mask(raster, buildings["geometry"])
     bounds = buildings.bounds
 
 main()
+
+"""
+To-dos:
+remove buildings in no data area
+"""

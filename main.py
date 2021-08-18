34a4f03ab9e5926226937fa8d15e6a8591d21910
Danner, Jonas <Danner@stud.uni-heidelberg.de>
2021-08-18 17:03:34 +0200

    added tiling and saving function in main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import geopandas as gpd
 from shapely.geometry import box
-import shapely
 import rasterio
 from rasterio import mask
 from rasterio.warp import calculate_default_transform, reproject, Resampling
@@ -9,7 +8,7 @@ import json
 import numpy as np
 
 
-from definitions import TRAINING_PATH, TEST_PATH, RASTER_PATH, INTERMEDIATE_PATH
+from definitions import TRAINING_PATH_IMG, TRAINING_PATH_MASK, TEST_PATH_IMG, TEST_PATH_MASK, RASTER_PATH, INTERMEDIATE_PATH
 from utils import query
 buildings_path = None
 
@@ -20,14 +19,6 @@ buil_def = {
          building = *
     """
 }
-
-def setup_folders():
-    if not os.path.exists(TRAINING_PATH):
-        os.mkdir(TRAINING_PATH)
-    if not os.path.exists(TEST_PATH):
-        os.mkdir(TEST_PATH)
-    if not os.path.exists(INTERMEDIATE_PATH):
-        os.mkdir(INTERMEDIATE_PATH)
 
 
 def reproject_raster():
@@ -55,10 +46,6 @@ def reproject_raster():
                     resampling=Resampling.nearest)
 
 
-
-    return None
-
-
 def get_building_data(raster):
     bbox = box(*raster.bounds)
     bbox = gpd.GeoSeries([bbox]).__geo_interface__
@@ -70,7 +57,7 @@ def get_building_data(raster):
 def generate_mask(raster, vector):
     out_image, out_transform = mask.mask(raster, vector, all_touched=False, invert=False, nodata=None, filled=True, crop=False,
                        pad=False, pad_width=0.5, indexes=None)
-    out_image = out_image[0] + out_image[1] + out_image[2]
+    out_image = np.sum(out_image, axis=0, dtype='uint8')
     out_image = np.where(out_image > 0, 1, out_image)
     out_meta = raster.meta
     out_meta.update({"driver": "GTiff",
@@ -81,22 +68,44 @@ def generate_mask(raster, vector):
 
     with rasterio.open(os.path.join(INTERMEDIATE_PATH, "masked_raster.tif"), "w", **out_meta) as dest:
         dest.write(out_image, 1)
-    return out_meta
+
+    r_mask = rasterio.open(os.path.join(INTERMEDIATE_PATH, "masked_raster.tif"))
+    return r_mask
+
+
+def crop_and_save(raster, bbox_feature, path, counter):
+    cropped_raster = mask.mask(raster, bbox_feature, crop=True)
+    if len(cropped_raster[0].shape) == 3 and np.sum(cropped_raster[0]) == 0:
+        return False
+    out_meta = raster.meta
+    out_meta.update({"height": cropped_raster[0].shape[-2],
+                     "width": cropped_raster[0].shape[-1],
+                     "transform": cropped_raster[1]})
+    with rasterio.open(os.path.join(path, f"sample_{counter}.tif"), "w",
+                       **out_meta) as dest:
+        dest.write(cropped_raster[0])
 
 
 def create_ml_data(raster, r_mask, vector):
     row_count = vector.shape[0]
-    train_max = round(row_count * 0.8)
-
 
     for index, row in vector.iterrows():
-        cropped_raster = mask.mask(raster, row, crop=True)
-        cropped_mask = mask.mask(r_mask, row, crop=True)
+        bbox_feature = box(*row)
+        bbox_feature = gpd.GeoSeries([bbox_feature])
 
+        if index % 5 == 0 and index != 0:
+            answer = crop_and_save(raster, bbox_feature, TEST_PATH_IMG, index)
+            if answer is False:
+                continue
+            crop_and_save(r_mask, bbox_feature, TEST_PATH_MASK, index)
+        else:
+            answer = crop_and_save(raster, bbox_feature, TRAINING_PATH_IMG, index)
+            if answer is False:
+                continue
+            crop_and_save(r_mask, bbox_feature, TRAINING_PATH_MASK, index)
 
 
 def main():
-    setup_folders()
     reproject_raster()
     raster = rasterio.open(os.path.join(INTERMEDIATE_PATH, "reprojected_raster.tif"))
     buildings = get_building_data(raster)
@@ -104,10 +113,7 @@ def main():
 
     r_mask = generate_mask(raster, buildings["geometry"])
     bounds = buildings.bounds
+    create_ml_data(raster, r_mask, bounds)
 
 main()
 
-"""
-To-dos:
-remove buildings in no data area
-"""

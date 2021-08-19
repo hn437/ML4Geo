ec39454c8e4ea6e8ac834ceea41c84e3b0e8801f
Danner, Jonas <Danner@stud.uni-heidelberg.de>
2021-08-19 10:03:47 +0200

    reformatted main.py using black

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
+import json
 import os
+
 import geopandas as gpd
-from shapely.geometry import box
+import numpy as np
 import rasterio
 from rasterio import mask
-from rasterio.warp import calculate_default_transform, reproject, Resampling
-import json
-import numpy as np
+from rasterio.warp import Resampling, calculate_default_transform, reproject
+from shapely.geometry import box
 
-
-from definitions import TRAINING_PATH_IMG, TRAINING_PATH_MASK, TEST_PATH_IMG, TEST_PATH_MASK, RASTER_PATH, INTERMEDIATE_PATH
+from definitions import (
+    INTERMEDIATE_PATH,
+    RASTER_PATH,
+    TEST_PATH_IMG,
+    TEST_PATH_MASK,
+    TRAINING_PATH_IMG,
+    TRAINING_PATH_MASK,
+    logger,
+)
 from utils import query
-buildings_path = None
+
 
 buil_def = {
-    "description":"All Buildings in an Area",
+    "description": "All Buildings in an Area",
     "endpoint": "elements/geometry",
     "filter": """
          building = *
-    """
+    """,
 }
 
 
 def reproject_raster():
-    dst_crs = 'EPSG:4326'
+    dst_crs = "EPSG:4326"
     with rasterio.open(RASTER_PATH) as src:
         transform, width, height = calculate_default_transform(
-            src.crs, dst_crs, src.width, src.height, *src.bounds)
+            src.crs, dst_crs, src.width, src.height, *src.bounds
+        )
         kwargs = src.meta.copy()
-        kwargs.update({
-            'crs': dst_crs,
-            'transform': transform,
-            'width': width,
-            'height': height
-        })
+        kwargs.update(
+            {"crs": dst_crs, "transform": transform, "width": width, "height": height}
+        )
 
-        with rasterio.open(os.path.join(INTERMEDIATE_PATH, "reprojected_raster.tif"), 'w', **kwargs) as dst:
+        with rasterio.open(
+            os.path.join(INTERMEDIATE_PATH, "reprojected_raster.tif"), "w", **kwargs
+        ) as dst:
             for i in range(1, src.count + 1):
                 reproject(
                     source=rasterio.band(src, i),
@@ -43,7 +51,8 @@ def reproject_raster():
                     src_crs=src.crs,
                     dst_transform=transform,
                     dst_crs=dst_crs,
-                    resampling=Resampling.nearest)
+                    resampling=Resampling.nearest,
+                )
 
 
 def get_building_data(raster):
@@ -55,18 +64,34 @@ def get_building_data(raster):
 
 
 def generate_mask(raster, vector):
-    out_image, out_transform = mask.mask(raster, vector, all_touched=False, invert=False, nodata=None, filled=True, crop=False,
-                       pad=False, pad_width=0.5, indexes=None)
-    out_image = np.sum(out_image, axis=0, dtype='uint8')
+    out_image, out_transform = mask.mask(
+        raster,
+        vector,
+        all_touched=False,
+        invert=False,
+        nodata=None,
+        filled=True,
+        crop=False,
+        pad=False,
+        pad_width=0.5,
+        indexes=None,
+    )
+    out_image = np.sum(out_image, axis=0, dtype="uint8")
     out_image = np.where(out_image > 0, 1, out_image)
     out_meta = raster.meta
-    out_meta.update({"driver": "GTiff",
-                     "count": int(1),
-                     "height": out_image.shape[0],
-                     "width": out_image.shape[1],
-                     "transform": out_transform})
+    out_meta.update(
+        {
+            "driver": "GTiff",
+            "count": int(1),
+            "height": out_image.shape[0],
+            "width": out_image.shape[1],
+            "transform": out_transform,
+        }
+    )
 
-    with rasterio.open(os.path.join(INTERMEDIATE_PATH, "masked_raster.tif"), "w", **out_meta) as dest:
+    with rasterio.open(
+        os.path.join(INTERMEDIATE_PATH, "masked_raster.tif"), "w", **out_meta
+    ) as dest:
         dest.write(out_image, 1)
 
     r_mask = rasterio.open(os.path.join(INTERMEDIATE_PATH, "masked_raster.tif"))
@@ -78,17 +103,21 @@ def crop_and_save(raster, bbox_feature, path, counter):
     if len(cropped_raster[0].shape) == 3 and np.sum(cropped_raster[0]) == 0:
         return False
     out_meta = raster.meta
-    out_meta.update({"height": cropped_raster[0].shape[-2],
-                     "width": cropped_raster[0].shape[-1],
-                     "transform": cropped_raster[1]})
-    with rasterio.open(os.path.join(path, f"sample_{counter}.tif"), "w",
-                       **out_meta) as dest:
+    out_meta.update(
+        {
+            "height": cropped_raster[0].shape[-2],
+            "width": cropped_raster[0].shape[-1],
+            "transform": cropped_raster[1],
+        }
+    )
+    with rasterio.open(
+        os.path.join(path, f"sample_{counter}.tif"), "w", **out_meta
+    ) as dest:
         dest.write(cropped_raster[0])
+    return True
 
 
 def create_ml_data(raster, r_mask, vector):
-    row_count = vector.shape[0]
-
     for index, row in vector.iterrows():
         bbox_feature = box(*row)
         bbox_feature = gpd.GeoSeries([bbox_feature])
@@ -105,15 +134,26 @@ def create_ml_data(raster, r_mask, vector):
             crop_and_save(r_mask, bbox_feature, TRAINING_PATH_MASK, index)
 
 
-def main():
-    reproject_raster()
+def main(from_file):
+    if not from_file:
+        reproject_raster()
     raster = rasterio.open(os.path.join(INTERMEDIATE_PATH, "reprojected_raster.tif"))
-    buildings = get_building_data(raster)
-    buildings = gpd.GeoDataFrame.from_features(buildings["features"])
+
+    if not from_file:
+        buildings = get_building_data(raster)
+        buildings = gpd.GeoDataFrame.from_features(buildings["features"])
+        buildings.to_file(
+            os.path.join(INTERMEDIATE_PATH, "buildings.geojson"), driver="GeoJSON"
+        )
+    else:
+        buildings = gpd.read_file(os.path.join(INTERMEDIATE_PATH, "buildings.geojson"))
+    logger.info(f"Number of buildings queried: {len(buildings.index)}")
 
     r_mask = generate_mask(raster, buildings["geometry"])
     bounds = buildings.bounds
     create_ml_data(raster, r_mask, bounds)
 
-main()
 
+if __name__ == "__main__":
+    from_file = False
+    main(from_file)

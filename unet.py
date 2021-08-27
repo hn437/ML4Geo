1c9122622c685316da791444e6e3135805c80281
Danner, Jonas <Danner@stud.uni-heidelberg.de>
2021-08-27 11:55:46 +0200

    created helping functions for plotting progress per epoch image in unet.py

--- unet.py
+++ unet.py
@@ -1,18 +1,33 @@
 import os
 
 import matplotlib.pyplot as plt
+import tensorflow.python.keras.callbacks
 
 from definitions import INTERMEDIATE_PATH, logger
 from main import BATCH_SIZE, EPOCH, TARGET_SIZE
 from model import build_model, get_generator
 
 
+def plot_fit_progress(history: tensorflow.python.keras.callbacks.History) -> None:
+    plt.figure()
+    plt.plot(range(EPOCH), history.history["accuracy"], label="Training Accuracy")
+    plt.plot(range(EPOCH), history.history["val_accuracy"], label="Validation Accuracy")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(os.path.join(INTERMEDIATE_PATH, "Progress_per_Epoch.png"))
+    logger.info("Fitting progress saved in png-File.")
+
+
+def calculate_confusion_matrix() -> None:
+    print()
+
+
 def unet_fit() -> None:
-    logger.info("Setting up generator...")
+    # Setting up generator
     train_gen, test_gen, no_of_trainsets, no_of_validsets = get_generator(
         batch_size=BATCH_SIZE, target_size=TARGET_SIZE
     )
-    logger.info("Build model and compile...")
+    # Build model and compile
     model = build_model(target_size=TARGET_SIZE)
 
     logger.info("Fit model...")
@@ -26,13 +41,8 @@ def unet_fit() -> None:
     model.save_weights(os.path.join(INTERMEDIATE_PATH, "weights.h5"))
     logger.info("Model weights saved!")
 
-    plt.figure()
-    plt.plot(range(EPOCH), history.history["accuracy"], label="Training Accuracy")
-    plt.plot(range(EPOCH), history.history["val_accuracy"], label="Validation Accuracy")
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig(os.path.join(INTERMEDIATE_PATH, "Progress_per_Epoch.png"))
-    logger.info("Fitting progress saved in png-File.")
+    plot_fit_progress(history)
+    calculate_confusion_matrix()
 
 
 if __name__ == "__main__":

0ac1d28f5fdc879186a489e38dea3a243a9eba0b
Danner, Jonas <Danner@stud.uni-heidelberg.de>
2021-09-02 17:22:50 +0200

    added functions to determine threshhold for best binary classification result, function to compute the confusion matrix, and a function to write a raster with the prediction results

--- unet.py
+++ unet.py
@@ -1,12 +1,18 @@
 import os
+import sys
 
+import tensorflow as tf
+import json
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, roc_auc_score, plot_roc_curve, roc_curve, auc
+from scipy.spatial.distance import cdist
 
-from definitions import INTERMEDIATE_PATH, TEST_PATH_IMG, logger
+from definitions import INTERMEDIATE_PATH, TEST_PATH_IMG, logger, RESULT_PATH, RASTER_PATH
+from utils import update_json, get_tiles
 from main import BATCH_SIZE, EPOCH, TARGET_SIZE
-from model import build_model, get_generator, get_generator_test
+from model import build_model, get_generator
+import rasterio
 
 
 def plot_fit_progress(history) -> None:
@@ -19,30 +25,120 @@ def plot_fit_progress(history) -> None:
     logger.info("Fitting progress saved in png-File.")
 
 
-def unet_evaluate() -> None:
+def determine_class_threshold() -> tuple:
     model = build_model(target_size=TARGET_SIZE)
-    model.load_weights(os.path.join(INTERMEDIATE_PATH, "weights.h5"))
-
-    no_of_testcases = len(os.listdir(TEST_PATH_IMG))
+    model.load_weights(os.path.join(RESULT_PATH, "weights.h5"))
 
     train_gen, test_gen, no_of_trainsets, no_of_validsets = get_generator(
-        batch_size=5, target_size=TARGET_SIZE
+        batch_size=BATCH_SIZE, target_size=TARGET_SIZE
     )
 
-    confusion_array = np.array([[0, 0], [0, 0]])
-
-    counter = 0
+    counter_processed_files = 0
+    mask_total = np.array([])
+    pred_total = np.array([])
 
     for image, mask in test_gen:
         predictions = model.predict(image, batch_size=1, workers=7)
         predictions = predictions.flatten()
         mask = mask.flatten()
-        confusion_array += confusion_matrix(mask, predictions)
-        counter += 1
-        if counter > 10:
+        mask_total = np.concatenate((mask_total, mask))
+        pred_total = np.concatenate((pred_total, predictions))
+
+        percentage = int((counter_processed_files + 1) / no_of_validsets * 100)
+        sys.stdout.write(f"\r Progress: {percentage} %. - {counter_processed_files} of {no_of_validsets} files predicted.")
+        sys.stdout.flush()
+        counter_processed_files += BATCH_SIZE
+        if counter_processed_files >= no_of_validsets:
             break
 
-    print(confusion_array)
+    with open(os.path.join(RESULT_PATH, "ground_truth.json"), "w") as file:
+        json.dump(mask_total, file)
+    with open(os.path.join(RESULT_PATH, "prediction.json"), "w") as file:
+        json.dump(pred_total, file)
+
+    fpr_total, tpr_total, thresholds_batch = roc_curve(mask_total, pred_total)
+    auc_keras = auc(fpr_total, tpr_total)
+
+    x = np.array([[0, 1]])
+    y = np.array([fpr_total, tpr_total]).transpose()
+    d = cdist(x, y)
+    idx = np.argmin(d)
+
+    threshold = thresholds_batch[idx]
+    print("Optimum threshold:", threshold)
+    update_json("threshold", threshold)
+
+    plt.figure(1)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr_total, tpr_total, label='Keras (area = {:.3f})'.format(auc_keras))
+    plt.scatter(y[idx][0], y[idx][1], c="black")
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend(loc='best')
+    plt.savefig(os.path.join(INTERMEDIATE_PATH, "ROC.png"))
+
+    return threshold, mask_total, pred_total
+
+
+def unet_evaluate() -> None:
+    threshold, mask_total, pred_total = determine_class_threshold()
+    pred_total = np.where(pred_total < threshold, 0, 1)
+    cm = confusion_matrix(mask_total, pred_total)
+
+    print(cm)
+    TP = cm[0][0]
+    FP = cm[0][1]
+    FN = cm[1][0]
+    TN = cm[1][1]
+
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    f1score = 2 * TP / (2 * TP + FP + FN)
+
+    update_json("precision", precision)
+    update_json("recall", recall)
+    update_json("accuracy", accuracy)
+    update_json("f1score", f1score)
+
+
+def predict_raster() -> None:
+    with open(os.path.join(RESULT_PATH, "metrics.json"), "r") as file:
+        threshold = json.load(file)["threshold"]
+    raster = rasterio.open(RASTER_PATH)
+
+    if os.path.exists(os.path.join(RESULT_PATH, "predicted_raster.tif")):
+        os.remove(os.path.join(RESULT_PATH, "predicted_raster.tif"))
+    out_meta = raster.meta.copy()
+    out_meta.update(
+        {
+            "driver": "GTiff",
+            "count": int(1),
+        }
+    )
+
+    model = build_model(target_size=TARGET_SIZE)
+    model.load_weights(os.path.join(RESULT_PATH, "weights.h5"))
+
+    for window, transform in get_tiles(raster, TARGET_SIZE[0], TARGET_SIZE[1]):
+        tile_data = raster.read(window=window)
+        tile_data = np.moveaxis(tile_data, 0, 2)
+        tile_data = tf.expand_dims(tile_data, axis=0)
+        predicted_tile = model.predict(tile_data, batch_size=1, workers=7)
+        predicted_tile = np.where(predicted_tile < threshold, 0, 1)
+        predicted_tile = tf.squeeze(predicted_tile, axis=0)
+        predicted_tile = np.moveaxis(predicted_tile, 2, 0)
+        if os.path.exists(os.path.join(RESULT_PATH, "predicted_raster.tif")):
+            with rasterio.open(
+                os.path.join(RESULT_PATH, "predicted_raster.tif"), "r+", BIGTIFF="YES", **out_meta
+            ) as outds:
+                outds.write(predicted_tile, window=window)
+        else:
+            with rasterio.open(
+                os.path.join(RESULT_PATH, "predicted_raster.tif"), "w", BIGTIFF="YES", **out_meta
+            ) as outds:
+                outds.write(predicted_tile, window=window)
 
 
 def unet_fit() -> None:
@@ -59,18 +155,24 @@ def unet_fit() -> None:
         validation_data=test_gen,
         steps_per_epoch=10,#(no_of_trainsets // BATCH_SIZE),
         epochs=EPOCH,
-        validation_steps=10,#(no_of_validsets // BATCH_SIZE),
+        validation_steps=10,#(no_of_validsets // BATCH_SIZE), #TODO change back
         workers=7,
         use_multiprocessing=False
     )
-    model.save_weights(os.path.join(INTERMEDIATE_PATH, "weights.h5"))
+    model.save_weights(os.path.join(RESULT_PATH, "weights.h5"))
     logger.info("Model weights saved!")
 
     plot_fit_progress(history)
 
+    determine_class_threshold()
     unet_evaluate()
+
+
+def unet_execution() -> None:
+    #unet_fit()
+    #unet_evaluate()
+    predict_raster()
 
 
 if __name__ == "__main__":
-    #unet_fit()
-    unet_evaluate()
+    unet_execution()

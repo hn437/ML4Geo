838bcf8d508ebc48f3ef9bb2879a39896b7fe459
Danner, Jonas <Danner@stud.uni-heidelberg.de>
2021-10-01 11:26:31 +0200

    reformatted main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from definitions import logger
 
-
 # SCRIPT SETTINGS:
 
+# Defining whether to use the new, working or the old, faulty workflow (in order to
+#  reproduce the failed experiment) for preprocessing
 NEW_WORKFLOW = True
+# the tile size which should be processed at once in preprocessing
 TILE_WIDTH = 25600
 TILE_HEIGHT = 25600
 
@@ -18,16 +20,21 @@ TARGET_SIZE = [256, 256]
 def main(mode: str) -> None:
     if mode == "Preprocessing":
         import preprocessing
+
         logger.info("Working Mode: Preprocess the data")
         preprocessing.preprocessing_data()
     elif mode == "Unet":
         import unet
+
         logger.info("Working Mode: Train the model and predict")
         unet.unet_execution()
     elif mode == "Complete":
         import preprocessing
         import unet
-        logger.info("Working Mode: Complete run, including preprocessing training and predicting")
+
+        logger.info(
+            "Working Mode: Complete run, including preprocessing training and predicting"
+        )
 
         logger.info("Doing the preprocessing")
         preprocessing.preprocessing_data()
@@ -39,7 +46,5 @@ def main(mode: str) -> None:
 
 
 if __name__ == "__main__":
-    #working_mode = "Preprocessing"
-    working_mode = "Unet"
-    #working_mode = "Complete"
+    working_mode = "Complete"
     main(working_mode)
